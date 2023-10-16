90e1c039718d3a9a3ead555db7bef01434bf9cf2
DanRoscigno <dan@roscigno.com>
2023-10-16 13:46:05 -0400

    update sidebars
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'documentation',
+            sidebarId: 'English',
             position: 'left',
             label: 'Documentation',
           },
